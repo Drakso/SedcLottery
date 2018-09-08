70ec7dbdeaa17e2db75067326b50425b1080edb1
Dejan Zdravkovski <dejan.zdravkovski@seavus.com>
2018-09-08 08:47:36 +0200

    [Lottery.Web] 3. and 4. day implementation.

--- Lottery.Web/Lottery.Data.Model/Migrations/Configuration.cs
+++ Lottery.Web/Lottery.Data.Model/Migrations/Configuration.cs
@@ -7,7 +7,7 @@ namespace Lottery.Data.Model.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(LotteryContext context)
