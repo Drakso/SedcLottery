16b5b76dc45a7505859f362578c5ea46aa737105
Risto Panchevski <risto.panchevski@outlook.com>
2018-10-04 17:56:16 +0200

    Bug fix

--- Lottery.RaffleConsole/Lottery.Service/LotteryManager.cs
+++ Lottery.RaffleConsole/Lottery.Service/LotteryManager.cs
@@ -121,7 +121,7 @@ namespace Lottery.Service
                 throw new ApplicationException("We are out of awards. Sorry!");
 
             var rnd = new Random();
-            var randomAwardIndex = rnd.Next(0, availableAwards.Count);
+            var randomAwardIndex = rnd.Next(0, availableAwards.Count - 1);
             return availableAwards[randomAwardIndex];
         }
     }
